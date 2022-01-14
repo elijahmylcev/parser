ce7222c9c577143caac21cddb803fc11e4aacfb1
Elijah Mylcev <elijahmylcev@gmail.com>
2022-01-14 22:05:33 +0300

    recurse function

--- parser.js
+++ parser.js
@@ -1,6 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const body = document.querySelector('body');
-	body.childNodes.forEach(node => {
-		console.log(node);
-	});
+	function recurse(element) {
+		element.childNodes.forEach(node => {
+			console.log(node);
+
+			if (element.childNodes.length > 1) {
+				recurse(node);
+			}
+		});
+	}
+
+	recurse(body);
 });
